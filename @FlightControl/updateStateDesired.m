ce02da51ca29c9588df72dbe2efd235333a6882c
Jan Thoemel <jan.thoemel@gmail.com>
2020-12-06 21:01:18 +0100

    visualizationLATLONRAD restructured
    -visualizationLATLONRAD renamed to ECEFprocessing
    -removed all GNSSR processing related functions; those are not functions of Simulations and are called from runSimulation
    -added a statistic analysis function hist3

--- @FlightControl/updateStateDesired.m
+++ @FlightControl/updateStateDesired.m
@@ -36,13 +36,13 @@ D=sqrt(3*SSC^2-2); %% if SSC==1 then D=1
 %ffp7 mean anomaly offset x,z [which dimension?]
 %ffp8 mean anomaly offset y   [which dimension?]
 
-sstDesired(1,:)= this.FFPS.ffp2 + B*meanMotionRad*this.FFPS.ffp6*time + this.FFPS.ffp1* cos( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7 )                               - this.FFPS.ffp5 *sqrt(2*SSC*A)*   sin( sqrt(2*SSC/A)*meanMotionRad* time  + this.FFPS.ffp7 );
-sstDesired(2,:)=                                                        this.FFPS.ffp3* cos( D*meanMotionRad*             time + this.FFPS.ffp8 )                               + this.FFPS.ffp4 /D/meanMotionRad* sin( D*meanMotionRad*             time  + this.FFPS.ffp8 );
-sstDesired(3,:)= this.FFPS.ffp6                                       + this.FFPS.ffp5* cos( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7 )                               + this.FFPS.ffp1 /sqrt(2*SSC*A)*   sin( sqrt(2*SSC/A)*meanMotionRad* time  + this.FFPS.ffp7 );
+sstDesired(1,:)= this.FFPS.ffp2 + B*meanMotionRad*this.FFPS.ffp6*time + this.FFPS.ffp1* cos( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7/180*pi )                               - this.FFPS.ffp5 *sqrt(2*SSC*A)*   sin( sqrt(2*SSC/A)*meanMotionRad* time  + this.FFPS.ffp7/180*pi);
+sstDesired(2,:)=                                                        this.FFPS.ffp3* cos( D*meanMotionRad*             time + this.FFPS.ffp8/180*pi )                               + this.FFPS.ffp4 /D/meanMotionRad* sin( D*meanMotionRad*             time  + this.FFPS.ffp8/180*pi );
+sstDesired(3,:)= this.FFPS.ffp6                                       + this.FFPS.ffp5* cos( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7/180*pi )                               + this.FFPS.ffp1 /sqrt(2*SSC*A)*   sin( sqrt(2*SSC/A)*meanMotionRad* time  + this.FFPS.ffp7/180*pi );
 
-sstDesired(4,:)=                  B*meanMotionRad*this.FFPS.ffp6      - this.FFPS.ffp1* sin( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7 ) *sqrt(2*SSC/A)*meanMotionRad  - this.FFPS.ffp5 *sqrt( 2*SSC*A)*  cos( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7 ) *sqrt(2*SSC/A)*meanMotionRad;
-sstDesired(5,:)=                                                      - this.FFPS.ffp3* sin( D*meanMotionRad*             time + this.FFPS.ffp8 ) *D*meanMotionRad              + this.FFPS.ffp4 /D/meanMotionRad* cos( D*meanMotionRad*             time + this.FFPS.ffp8 ) *D*meanMotionRad;
-sstDesired(6,:)=                                                      - this.FFPS.ffp5* sin( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7 ) *sqrt(2*SSC/A)*meanMotionRad  + this.FFPS.ffp1 /sqrt(2*SSC*A)*   cos( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7 ) *sqrt(2*SSC/A)*meanMotionRad;
+sstDesired(4,:)=                  B*meanMotionRad*this.FFPS.ffp6      - this.FFPS.ffp1* sin( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7/180*pi ) *sqrt(2*SSC/A)*meanMotionRad  - this.FFPS.ffp5 *sqrt( 2*SSC*A)*  cos( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7/180*pi ) *sqrt(2*SSC/A)*meanMotionRad;
+sstDesired(5,:)=                                                      - this.FFPS.ffp3* sin( D*meanMotionRad*             time + this.FFPS.ffp8/180*pi ) *D*meanMotionRad              + this.FFPS.ffp4 /D/meanMotionRad* cos( D*meanMotionRad*             time + this.FFPS.ffp8/180*pi ) *D*meanMotionRad;
+sstDesired(6,:)=                                                      - this.FFPS.ffp5* sin( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7/180*pi ) *sqrt(2*SSC/A)*meanMotionRad  + this.FFPS.ffp1 /sqrt(2*SSC*A)*   cos( sqrt(2*SSC/A)*meanMotionRad* time + this.FFPS.ffp7/180*pi ) *sqrt(2*SSC/A)*meanMotionRad;
 
 %%^^^^^^ Re-implement formation mode selection...
 % Implement based on switches
